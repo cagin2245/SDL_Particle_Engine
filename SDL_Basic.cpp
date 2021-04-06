98ab8e2bf3a6565069f9941ac04f780d533497b1
Çağın Kılınç <caginkilincyt@gmail.com>
2021-04-06 05:38:16 +0300

    RGB transition

--- SDL_Basic.cpp
+++ SDL_Basic.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <SDL.h>
+#include <math.h>
 #include "Ekran.h"
 #undef main
 
@@ -22,15 +23,20 @@ int main()
 	while (true) // Game Loop
 	{	// Update Particles
 		// Draw Particles
+		int elapsed = SDL_GetTicks();
+		unsigned char green = (unsigned char)((1 + sin(elapsed * 0.0053)) * 128);
+		unsigned char red = (unsigned char)((1 + sin(elapsed * 0.0023)) * 128);
+		unsigned char blue = (unsigned char)((1 + sin(elapsed * 0.0042)) * 128);
+
 		for (int y = 0; y < Ekran::EKRAN_UZUNLUGU; y++){	
 			for (int x = 0; x < Ekran::EKRAN_GENISLIGI; x++)
 			{
-				ekran.setPixel(x, y, 128, 0, 255);
+				ekran.setPixel(x, y, red, green, blue);
 			}
 		}
 		 
 		// Check for messages/events
-		
+		ekran.setPixel(400, 300, 255, 255, 255);
 		ekran.update();// Draw the screen
 
 
